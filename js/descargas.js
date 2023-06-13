c950187b272dcb926686aa142eaaf4bc2033a0c6
loadigital <95947050+loadigital@users.noreply.github.com>
2023-06-12 21:52:34 -0500

    Update descargas.js

--- js/descargas.js
+++ js/descargas.js
@@ -44,7 +44,7 @@ const portadaIndex = data =>{
      <p>Totalmente gratis</p>
      </div>
      <div class="co-des-a">
-     <a href="B!LOA-5.0.1.apk" target="_blank">DESCARGALA AHORA</a>
+     <a href="B!LOA-5.0.2.apk" target="_blank" class="bi-android2" > Descargar APP</a>
      </div>
      </div>
      
